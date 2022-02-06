0e18473227b1f4fef86b521ef10e82035196f709
Ben Webb <ben@salilab.org>
2022-02-05 21:37:00 -0800

    Skip compile tests when cross-compiling

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,8 @@
+# Skip compile tests if we are cross-compiling
+if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
+  exit 0
+fi
+
 # Test boost::iostreams zlib filter support
 cd test
 ${CXX} -I$PREFIX/include -L$PREFIX/lib test_iostreams_zlib.cpp -lboost_iostreams
