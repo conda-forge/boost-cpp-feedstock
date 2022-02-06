e0a309637954cb4121298ed058d869b8aaf198f1
Ben Webb <ben@salilab.org>
2022-02-05 22:10:27 -0800

    Fix Boost linkage on Windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,4 +1,4 @@
 :: Test boost::iostreams zlib filter support
 cd test
-cl.exe /EHsc /I%PREFIX%\Library\include test_iostreams_zlib.cpp /link /libpath:%PREFIX%\Library\lib boost_iostreams.lib
+cl.exe /EHsc /MD /DBOOST_ALL_DYN_LINK /DBOOST_ZLIB_BINARY=kernel32 /I%PREFIX%\Library\include test_iostreams_zlib.cpp /link /libpath:%PREFIX%\Library\lib
 if errorlevel 1 exit 1
